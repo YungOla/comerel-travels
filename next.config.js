15aa2ae753eb7f220b12e00651697faeea7366c3
YungOla <abdulsalamniyi@gmail.com>
2026-08-26 12:26:46 +0100

    Improve Netlify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: true,
 };
 
 module.exports = nextConfig;
